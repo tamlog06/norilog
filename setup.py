36db09597b5cf1ff97e3b706163d9d798c2fd2b0
tam1006 <tamusyun1006@gmail.com>
2021-05-02 16:54:59 +0900

    modify setup.py

--- setup.py
+++ setup.py
@@ -1,10 +1,32 @@
+import os
 from setuptools import setup, find_packages
+
+def read_file(filename):
+    basepath = os.path.dirname(os.path.dirname(__file__))
+    filepath = os.path.join(basepath, filename)
+    if os.path.exists(filepath):
+        return open(filepath).read()
+    else:
+        return ''
 
 setup(
     name='norilog',
     version='1.0.0',
+    description='The NoriLog web application.',
+    long_description=read_file('README.rst'),
+    author='tam1006',
+    author_email='tamusyun1006@gmail.com',
+    url='https://github.com/tam1006/norilog/',
+    classifiers=[
+        'Development Status :: 4 - Beta',
+        'Framework :: Flask',
+        'Licence :: OSI Approved :: BSD Licence',
+        'Programming Language :: Python',
+    ],
     packages=find_packages(),
     include_package_data=True,
+    keywords=['web', 'norilog'],
+    license='BSD License',
     install_requires=[
         'Flask',
     ],
